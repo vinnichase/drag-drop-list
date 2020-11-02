47feba58447a01dc8945adc0089b1c122f04c900
vinnichase <vincenz.koop@gmail.com>
2020-11-02 18:25:28 +0100

    linear drag scrolling

--- src/index.js
+++ src/index.js
@@ -1,84 +1,47 @@
 import { render } from 'react-dom'
-import React, { useState, useRef, useEffect } from 'react'
-import { useTransition, animated, useSpring } from 'react-spring'
-// import { Spring, animated as anim } from 'react-spring/renderprops'
-import { useMove, useScroll } from 'react-use-gesture'
+import React, { useState, useRef } from 'react'
+import { useTransition, animated } from 'react-spring'
+import { useDrag } from 'react-use-gesture'
 import data from './data'
 import './styles.css'
 import useWindowDimensions from './useWindowDimensions'
+import useInterval from './useInterval'
 
 function App() {
   const [scroll, setScroll] = useState(0)
-  const [currentScrollTop, setCurrentScrollTop] = useState(0)
-  const [scrollHeight, setScrollHeight] = useState(0)
+  const [isDragging, setIsDragging] = useState(false)
   const [, vh] = useWindowDimensions()
   const container = useRef(null)
-  useEffect(() => {
-    if (container.current) {
-      setScrollHeight(container.current.scrollHeight - container.current.getBoundingClientRect().bottom)
-      setCurrentScrollTop(container.current.scrollTop)
-    }
-  }, [container])
-  const scrollSpringConfig = (mass = 12, tension = 12, friction = 19, precision = 0.01, clamp = false) => ({
-    mass,
-    tension,
-    friction,
-    precision,
-    clamp
-  })
-  const [scrollSpring, setScrollSpring, stop] = useSpring(() => ({
-    reset: true,
-    config: scrollSpringConfig()
-  }))
 
-  const bind = useMove(({ xy: [, y] }) => {
-    if (scroll > -1 && y < 0.15 * vh) {
-      setScroll(-1)
-      setScrollSpring({
-        from: {
-          y: currentScrollTop
-        },
-        to: { y: 0 },
-        config: scrollSpringConfig()
-      })
+  useInterval(() => {
+    if (scroll !== 0) {
+      container.current.scrollTop += scroll * 10
     }
-    if (scroll !== 0 && y >= 0.15 * vh && y <= 0.85 * vh) {
-      console.log(currentScrollTop, scrollSpring.y && Math.floor(scrollSpring.y.getValue()))
+  }, 10)
+
+  const bind = useDrag(({ xy: [, y], dragging }) => {
+    setIsDragging(dragging)
+    if (dragging) {
+      const boundsSize = 0.15 * vh;
+      const topBounds = boundsSize;
+      const bottomBounds = vh - boundsSize;
+      if (y < topBounds) {
+        setScroll((y - topBounds) / boundsSize)
+      }
+      if (scroll !== 0 && y >= topBounds && y <= bottomBounds) {
+        setScroll(0)
+      }
+      if (y > bottomBounds) {
+        setScroll((y - bottomBounds) / boundsSize)
+      }
+    } else {
       setScroll(0)
-      setScrollSpring({
-        from: {
-          y: currentScrollTop
-        },
-        to: {
-          y: scrollSpring.y ? Math.floor(scrollSpring.y.getValue()) : currentScrollTop
-        },
-        config: scrollSpringConfig(12, 1, 100),
-        onRest: () => console.log('REST'),
-        onStart: () => console.log('START')
-      })
     }
-    if (scroll < 1 && y > 0.85 * vh) {
-      setScroll(1)
-      setScrollSpring({
-        from: {
-          y: currentScrollTop
-        },
-        to: { y: scrollHeight },
-        config: scrollSpringConfig()
-      })
-    }
+  }, {
+    passive: false
   })
-  useScroll(
-    ({ xy: [, y] }) => {
-      console.log(y)
-      container.current && setCurrentScrollTop(container.current.scrollTop)
-    },
-    {
-      domTarget: container,
-      passive: true
-    }
-  )
 
+  /* #region   */
   const [rows, set] = useState(data)
   let height = 0
   const transitions = useTransition(
@@ -91,25 +54,25 @@ function App() {
       update: ({ y, height }) => ({ y, height })
     }
   )
+  /* #endregion */
 
   return (
-    <div className="scrolltop-main">
-      <animated.div {...bind()} ref={container} className="list-container" scrollTop={scrollSpring.y}>
-        <div className="list" style={{ height }}>
-          {transitions.map(({ item, props: { y, ...rest }, key }, index) => (
-            <animated.div
-              key={key}
-              className="card"
-              style={{
-                zIndex: data.length - index,
-                transform: y.interpolate((y) => `translate3d(0,${y}px,0)`),
-                ...rest
-              }}>
-              <div className="details" style={{ backgroundImage: item.css }} />
-            </animated.div>
-          ))}
-        </div>
-      </animated.div>
+    <div ref={container} className="list-container">
+      <div className="list" style={{ height }}>
+        {transitions.map(({ item, props: { y, ...rest }, key }, index) => (
+          <animated.div
+            {...bind()}
+            key={key}
+            className="card"
+            style={{
+              zIndex: data.length - index,
+              transform: y.interpolate((y) => `translate3d(0,${y}px,0)`),
+              ...rest
+            }}>
+            <div className="details" style={{ backgroundImage: item.css }} />
+          </animated.div>
+        ))}
+      </div>
     </div>
   )
 }
